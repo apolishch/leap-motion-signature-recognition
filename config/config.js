58bc136cf8dee4ed577a8e40dc38d12d338f4aa7
Abraham <abe@gobalto.com>
2013-08-06 01:41:52 -0700

    WIP

--- config/config.js
+++ config/config.js
@@ -1,4 +1,10 @@
-module.exports = function(app){
-    app.set('views', __dirname + '../views');
+module.exports = function(app, express){
+    app.set('views', __dirname + '/../views');
     app.engine('html', require('ejs').renderFile);
+    app.use(express.static(__dirname + '/../public'));
+    app.use(require('asset-pipeline')({
+        server: app,
+        assets: '/../public',
+        cache: '/../cache'
+    }));
 }
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-require('./config/config.js')(app);
+require('./config/config.js')(app, express);
 require('./config/routes.js')(app);
 
 app.listen(7777);
