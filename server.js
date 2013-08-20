4ac1106f2c7cac415bca0070916f54207a373409
Abraham <abe@gobalto.com>
2013-08-20 01:49:14 -0700

    removing dead socket

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
 var express = require('express');
 var app = express();
 
-
-ws.on('message', function(data, flags) {
-    console.log(data);
-});
-
 require('./config/config.js')(app, express);
 require('./config/routes.js')(app);
 
